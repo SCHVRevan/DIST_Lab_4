8069c8f0d3ef83fb7fec2aae84a72e8eaeae1ddb
Egor <120785170+SCHVRevan@users.noreply.github.com>
2025-06-10 13:41:37 +0700

    Update cipher.cpp
    took out the main

--- cipher.cpp
+++ cipher.cpp
@@ -1,10 +1,11 @@
 #include <openssl/aes.h>
 #include <fstream>
 #include <vector>
-#include "dir_checker.cpp"
+#include "dir_checker.h"
+#include "cipher.h"
 using namespace std;
 
-bool encrypt(const string& file, const string& key) {
+bool cipher::encrypt(const string& file, const string& key) {
     ifstream infile(file, ios::binary);
     vector<unsigned char> data((istreambuf_iterator<char>(infile)), istreambuf_iterator<char>());
     
@@ -22,13 +23,13 @@ bool encrypt(const string& file, const string& key) {
     return 1;
 }
 
-bool decrypt(const string& file, const string& key) {
+bool cipher::decrypt(const string& file, const string& key) {
     ifstream infile(file, ios::binary);
     vector<unsigned char> data((istreambuf_iterator<char>(infile)), istreambuf_iterator<char>());
     
     AES_KEY aesKey;
     // reinterpret_cast может быть небезопасным (его поведение зависит от реализации компилятора)
-    AES_set_encrypt_key(reinterpret_cast<const unsigned char*>(key.c_str()), 256, &aesKey);
+    AES_set_decrypt_key(reinterpret_cast<const unsigned char*>(key.c_str()), 256, &aesKey);
     
     unsigned char iv[AES_BLOCK_SIZE] = {0};
     vector<unsigned char> out(data.size());
@@ -38,29 +39,4 @@ bool decrypt(const string& file, const string& key) {
     ofs.write(reinterpret_cast<char*>(out.data()), out.size());
 
     return 1;
-}
-
-int main(int argc, char* argv[]) {
-    if (argc != 4) {
-        cout << "Usage:\n" << argv[0] << " <path> <mode (enc/dec)> <key>\n";
-        return 1;
-    }
-
-    string key = argv[3];
-    vector<string> f;
-    f = get_files(argv[1]);
-    bool check;
-
-    if (argv[2] == "enc") {
-        check = encrypt(f[0], key);
-    }
-    else if (argv[2] == "dec") {
-        check = decrypt(f[0], key);
-    }
-    else {
-        cout << "Usage:\n" << argv[0] << " <path> <mode (enc/dec)> <key>\n";
-        return 1;
-    }
-
-    return 0;
 }
