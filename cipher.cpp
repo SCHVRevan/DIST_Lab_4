1019c3dcfa5a66ba270aba6af64cb04812a80979
Egor <120785170+SCHVRevan@users.noreply.github.com>
2025-06-10 18:37:31 +0700

    Update cipher.cpp
    depadding in the decoder

--- cipher.cpp
+++ cipher.cpp
@@ -1,17 +1,16 @@
 #include <openssl/aes.h>
 #include <fstream>
-#include <vector>
 #include "dir_checker.h"
 #include "cipher.h"
 using namespace std;
 
 bool cipher::encrypt(const string& file, const string& key) {
     ifstream infile(file, ios::binary);
-    vector<unsigned char> data((istreambuf_iterator<char>(infile)), istreambuf_iterator<char>());
+    vector<unsigned char> data((istreambuf_iterator<char>(infile)), {});
     
     int pad = AES_BLOCK_SIZE - data.size() % AES_BLOCK_SIZE;
     data.insert(data.end(), pad, pad);
-
+    
     AES_KEY aesKey;
     // reinterpret_cast может быть небезопасным (его поведение зависит от реализации компилятора)
     AES_set_encrypt_key(reinterpret_cast<const unsigned char*>(key.c_str()), 256, &aesKey);
@@ -28,7 +27,7 @@ bool cipher::encrypt(const string& file, const string& key) {
 
 bool cipher::decrypt(const string& file, const string& key) {
     ifstream infile(file, ios::binary);
-    vector<unsigned char> data((istreambuf_iterator<char>(infile)), istreambuf_iterator<char>());
+    vector<unsigned char> data((istreambuf_iterator<char>(infile)), {});
     if (data.size() % AES_BLOCK_SIZE) return 0;
 
     AES_KEY aesKey;
@@ -38,7 +37,10 @@ bool cipher::decrypt(const string& file, const string& key) {
     unsigned char iv[AES_BLOCK_SIZE] = {0};
     vector<unsigned char> out(data.size());
     AES_cbc_encrypt(data.data(), out.data(), data.size(), &aesKey, iv, AES_DECRYPT);
-
+    
+    int pad = out.back();
+    out.resize(out.size() - pad);
+    
     ofstream ofs(file, ios::binary);
     ofs.write(reinterpret_cast<char*>(out.data()), out.size());
 
