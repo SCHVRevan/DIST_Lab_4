b894fb8468b9549de9048b0bc76ad3919a20ff1c
Egor <120785170+SCHVRevan@users.noreply.github.com>
2025-06-10 13:44:57 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream> 
+#include <string>
+#include "dir_checker.h"
+#include "cipher.h"
+
+int main(int argc, char* argv[]) {
+    if (argc != 4) {
+        std::cout << "Usage:\n" << argv[0] << " <path> <mode (enc/dec)> <key>\n";
+        return 1;
+    }
+
+    std::string key = argv[3];
+    std::vector<std::string> f;
+    f = dir_checker::get_files(argv[1]);
+    
+    if (static_cast<std::string>(argv[2]) == "enc") {
+        cipher::encrypt(f[0], key);
+    }
+    else if (static_cast<std::string>(argv[2]) == "dec") {
+        cipher::decrypt(f[0], key);
+    }
+    else {
+        std::cout << "Usage:\n" << argv[0] << " <path> <mode (enc/dec)> <key>\n";
+        return 1;
+    }
+
+    return 0;
+}
